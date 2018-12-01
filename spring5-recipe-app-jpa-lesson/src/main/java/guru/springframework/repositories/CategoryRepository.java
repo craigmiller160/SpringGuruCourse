e2c3877fb1fd6034b39b80faaae5117f3169d160
Craig Miller <craigmiller160@gmail.com>
2018-12-01 15:39:51 -0500

    Created Spring Repositories.

---
+++ spring5-recipe-app-jpa-lesson/src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.repositories;
+
+import guru.springframework.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+}
