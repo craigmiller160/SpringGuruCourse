53b60f17c67a174b1535352bed1d3064c216c570
Craig Miller <craigmiller160@gmail.com>
2018-12-01 11:50:47 -0500

    Learned about the default profile

--- di-demo/src/main/java/io/craigmiller160/spring/didemo/services/PrimaryGermanGreetingService.java
+++ di-demo/src/main/java/io/craigmiller160/spring/didemo/services/PrimaryGermanGreetingService.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"default", "en"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - from the primary greeting service";
+        return "Primarer Grubdienst";
     }
 }
