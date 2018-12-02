f12ecfe2d81cf8211a99152673603128826d816a
Craig Miller <craigmiller160@gmail.com>
2018-12-02 12:10:56 -0500

    Practiced using spring data jpa query methods in CrudRepository.

--- spring5-recipe-app-jpa-lesson/src/main/java/guru/springframework/controllers/IndexController.java
+++ spring5-recipe-app-jpa-lesson/src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.Category;
+import guru.springframework.model.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +15,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> category = categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByDescription("Cup");
+
+        System.out.println("Cat Id is: " + category.get().getId()); //2
+        System.out.println("UOM Id is: " + uom.get().getId()); //3
+
         return "index";
     }
 }
