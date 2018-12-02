58547d7acadd17cc69ab17db0d2bf7beb245daff
Craig Miller <craigmiller160@gmail.com>
2018-12-02 12:54:37 -0500

    Added Lombok to some more classes.

---
+++ spring5-recipe-app-jpa-lesson/src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -0,0 +1,29 @@
+package guru.springframework.services;
+
+import guru.springframework.model.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Slf4j
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+        return StreamSupport.stream(recipeRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+}
